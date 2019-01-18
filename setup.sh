ea63abe08326bbaa91d90b20d0f8653788f47ea5
Marcos <vmarcosp.pereira@gmail.com>
2019-01-18 09:05:59 -0200

    Add firacode.sh and gitignore

--- setup.sh
+++ setup.sh
@@ -1,20 +1,7 @@
-# Create setups folder
-mkdir _setups
-
 # Code extensions
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension jpoissonnier.vscode-styled-component
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension obinbentley.sass-indented
-code --install-extension silvenon.mdx
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension dracula-theme.theme-dracula
 
 # Mv git config
 cp .gitconfig ~/
-
-# Install Monaco font
-bash fonts/monaco.sh
 
 # Fix NVM/Node ZSH
 bash nvm-zsh.sh
