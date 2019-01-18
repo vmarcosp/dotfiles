4f98d2b62114eadccddd7cc4977f6f13f035411e
Marcos <vmarcosp.pereira@gmail.com>
2019-01-18 10:11:29 -0200

    *

--- code_test.sh
+++ code_test.sh
@@ -1,15 +1,6 @@
-# Code extensions
 code --install-extension mikestead.dotenv
 code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension file-icons.file-icons
-code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension freebroccolo.reasonml
 code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension jpoissonnier.vscode-styled-components
-
-# Mv git config
-cp .gitconfig ~/
-
-# Fix NVM/Node ZSH
-bash nvm-zsh.sh
