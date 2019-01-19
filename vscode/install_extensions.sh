764263163b17aeb314052a69e2e656f2b024a7b8
vmarcosp <vmarcosp.pereira@gmail.com>
2019-01-18 22:56:30 -0200

    Fix script

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -1,9 +1,9 @@
 # Code extensions
+
 code --install-extension mikestead.dotenv
 code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension file-icons.file-icons
-code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension freebroccolo.reasonml
 code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension whizkydee.material-palenight-theme
