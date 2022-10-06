fec7c7825fd58e7eddac3eeb1fe07afe8ece9f7c
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-10-06 12:08:37 -0300

    *

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,16 +2,16 @@ local wezterm = require 'wezterm'
 
 return {
   font = wezterm.font('SpaceMono Nerd Font Mono', { weight = "Bold" }),
-  font_size = 20,
+  font_size = 18,
 	use_fancy_tab_bar = false,
-  line_height = 1.6,
+  line_height = 1.45,
 	hide_tab_bar_if_only_one_tab = true,
   color_scheme = "Catppuccin Mocha",
 	window_decorations = "RESIZE",
   window_padding = {
-		left = 24,
-		right = 24,
-		top = 32,
-		bottom = 24,
+		left = 36,
+		right = 36,
+		top = 42,
+		bottom = 32,
 	},
 }
