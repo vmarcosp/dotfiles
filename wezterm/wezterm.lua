9d1a63b34b5737690a9d95ef98109512af0c7d50
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-10-13 08:10:39 -0300

    chore: updating better vim config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   font = wezterm.font('SpaceMono Nerd Font Mono', { weight = "Bold" }),
-  font_size = 18,
+  font_size = 16,
 	use_fancy_tab_bar = false,
   line_height = 1.7,
 	hide_tab_bar_if_only_one_tab = true,
