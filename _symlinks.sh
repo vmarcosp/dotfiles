63f2ae5a0551802f97e97799891d0a7445dcf946
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-03-13 09:00:13 -0300

    Delete _symlinks.sh

--- _symlinks.sh
+++
@@ -1,4 +0,0 @@
-ln -sf ~/Projects/dotfiles/better-vim/overrides.vim ~/.config/better-vim/
-ln -sf ~/Projects/dotfiles/better-vim/plugins.vim ~/.config/better-vim/
-ln -sf ~/Projects/dotfiles/better-vim/better-vim.lua ~/.config/better-vim/
-ln -sf ~/Projects/dotfiles/better-vim/coc/coc-settings.json ~/.config/nvim/
