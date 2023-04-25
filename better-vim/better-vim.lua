57acabe1b886956cac78c4b0df9bbc28ff365660
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-04-25 08:16:38 -0300

    nvim: added better vim config for v3

--- better-vim/better-vim.lua
+++ better-vim/better-vim.lua
@@ -2,17 +2,30 @@ return {
   mappings = {
     leader = "\\",
   },
+  plugins = {
+    "rescript-lang/vim-rescript",
+    "nkrkv/nvim-treesitter-rescript",
+    "devongovett/tree-sitter-highlight",
+  },
+  treesitter = "all",
+  lsps = {
+    ["rescriptls@latest-master"] = {},
+  },
   theme = {
     name = "catppuccin",
-    catppuccin_flavour = "frappe"
   },
   flags = {
     disable_tabs = true
   },
   nvim_tree = {
     view = {
-		  hide_root_folder = true,
-      adaptive_size = true
+      hide_root_folder = true,
+      adaptive_size = false
     }
   },
+  hooks = {
+    after_setup = function()
+      vim.cmd("set relativenumber")
+    end
+  }
 }
