2cabd2ea546b8b3680e60ab0aba08169ca0739e4
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-05-11 16:30:12 -0300

    chore(better-vim): improving config composition

--- better-vim/better-vim.lua
+++ better-vim/better-vim.lua
@@ -1,35 +1,42 @@
-return {
-  mappings = {
-    leader = "\\",
-  },
-  plugins = {
-    "rescript-lang/vim-rescript",
-    "nkrkv/nvim-treesitter-rescript",
-    "devongovett/tree-sitter-highlight",
-  },
-  lsps = {},
-  theme = {
-    name = "catppuccin",
-    catppuccin_flavour = "macchiato",
-  },
-  flags = {
-    format_on_save = true,
-    disable_tabs = true
-  },
-  nvim_tree = {
-    view = {
-      adaptive_size = false
-    }
-  },
-  lualine = {
-    options = {
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
-    }
-  },
-  hooks = {
-    after_setup = function()
-      vim.cmd("set relativenumber")
-    end
+local M = {}
+
+M.plugins = {
+  "rescript-lang/vim-rescript",
+  "nkrkv/nvim-treesitter-rescript",
+  "devongovett/tree-sitter-highlight",
+}
+
+M.mappings = {
+  leader = "\\",
+}
+
+M.theme = {
+  name = "catppuccin",
+  catppuccin_flavour = "macchiato",
+}
+
+M.flags = {
+  format_on_save = true,
+  disable_tabs = true
+}
+
+M.nvim_tree = {
+  view = {
+    adaptive_size = false
   }
 }
+
+M.lualine = {
+  options = {
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  }
+}
+
+M.hooks = {
+  after_setup = function()
+    vim.cmd("set relativenumber")
+  end
+}
+
+return M
