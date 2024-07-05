7c026b0da5782218d281027e819e5f4b07e2a268
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-07-05 17:29:34 -0300

    *

--- tmux/shifter.sh
+++ tmux/shifter.sh
@@ -1,19 +1,9 @@
 # Colors
-WINDOW_FORMAT=" #I: #W "
 BACKGROUND="#000000"
-PRIMARY="#151515"
 FOREGROUND="#D2D2D2"
 
 # Window
 tmux set -g window-status-separator ""
-
-## Current
-tmux set -g window-status-current-format "$WINDOW_FORMAT"
-tmux set -g window-status-current-style "bg=$PRIMARY"
-
-## General
-tmux set -g window-status-style "bg=$BACKGROUND, fg=$FOREGROUND"
-tmux set -g window-status-format "$WINDOW_FORMAT"
 
 # Status
 tmux set -g status-bg "$BACKGROUND"
