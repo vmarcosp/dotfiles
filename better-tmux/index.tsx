026b6ba0c358b13961a1ed95d5c7fca49e302562
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-07-08 10:38:07 -0300

    chore: added missing fg config

--- better-tmux/index.tsx
+++ better-tmux/index.tsx
@@ -3,14 +3,15 @@ import { Box, BetterTmuxConfig, WindowConfig } from 'better-tmux'
 const Window = ({ type, number, name }: WindowConfig) => {
   return (
     <Box padding={1} bg={type === 'active' ? '#151515' : '#000000'}>
-      {number}: {name}
+      {number}/ {name}
     </Box>
   )
 }
 
 export default {
   status: {
-    bg: '#000000'
+    bg: '#000000',
+    fg: '#fafafa'
   },
   window: (window) => <Window {...window} />
 } satisfies BetterTmuxConfig
