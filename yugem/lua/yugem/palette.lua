6da194e36bb51c96523630b43651a4d923b8dc27
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-10-01 19:45:55 -0300

    **

--- yugem/lua/yugem/palette.lua
+++ yugem/lua/yugem/palette.lua
@@ -1,27 +1,6 @@
 local variants = {
   main = {
-    yellow = '#FFFF00',
-    teal1 = '#008080',
-    teal2 = '#FFD700',
-    teal3 = '#FF4500',
-    blue1 = '#0000FF',
-    blue2 = '#00FF00',
-    blue3 = '#FF00FF',
-    blue4 = '#00FFFF',
-    pink1 = '#FFC0CB',
-    pink2 = '#000000',
-    pink3 = '#FC3666',
-    blueGray1 = '#A52A2A',
-    blueGray2 = '#CACACA',
-    blueGray3 = '#8A2BE2',
-    background1 = '#000000',
-    background2 = '#FFFFFF',
-    background3 = '#FF0000',
-    text = '#FAFAFA',
-    white = '#800080',
-    none = 'NONE',
-
-    -- new theme
+    placeholder = '#FFFF00',
     color100 = '#FAFAFA',
     color200 = '#D4D4D4',
     color300 = '#A9A9A9',
@@ -33,7 +12,8 @@ local variants = {
     primary = '#FFBE89',
     success = '#7EAB8E',
     warning = '#FFF2AF',
-    error = '#F57A7A'
+    error = '#F57A7A',
+    none = 'none'
   },
 }
 
