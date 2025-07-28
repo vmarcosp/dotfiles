a942fbef2042decc25dab140d4b49525f98784f1
Marcos Oliveira <marcos.oliveira@vtex.com>
2025-07-28 09:44:42 -0300

    *

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -41,3 +41,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
