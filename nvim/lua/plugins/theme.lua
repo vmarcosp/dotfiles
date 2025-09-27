de2ad76d0549625ae9b1398c6d02db457444b417
Marcos Oliveira <marcos.oliveira@vtex.com>
2025-09-27 00:17:16 -0300

    *

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,7 +2,8 @@ local custom_catppuccin = require("plugins/themes/custom_catppuccin")
 
 return {
 	-- custom_catppuccin,
-	"bettervim/yugen.nvim",
+	-- "bettervim/yugen.nvim",
+	dir = "~/projects/vmcp/yugen.nvim",
 	config = function()
 		vim.cmd.colorscheme("yugen")
 	end,
